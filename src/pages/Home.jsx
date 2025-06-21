2d1749cbbc2ea4171bd3d49a51e335094abfe37e
Gloria Dedo <gloriadedo100@gmail.com>
2025-06-21 09:57:36 -0700

    feat: added pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import HeroSection from "@/components/HeroSection"
 import AboutUs from "@/components/AboutUs"
 export default function Home() {
     return (
-        <div className="bg-darkest-heading w-full h-full">
+        <div className="bg-darkest-heading w-full h-full ">
             <HomeNav />
             <HeroSection />
             <div className="min-h-screen bg-[url('./assets/argriculturCover.jpg')] bg-center bg-cover relative">
