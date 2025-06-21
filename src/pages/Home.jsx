77d84747475d04c8ba57f55d2570eb6473dba1d7
Gloria Dedo <gloriadedo100@gmail.com>
2025-06-20 23:14:34 -0700

    added assets and fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import {  Flex, Text, Button } from "@radix-ui/themes"
 export default function Home(){
     return(
         <div>
-            <p className="bg-amber-950 text-shadow-indigo-300 font-extrabold text-9xl font-lato">This is Home</p>
+            <p className="text-red-400 text-shadow-indigo-300 font-extrabold text-9xl font-poppins">This is Home</p>
             <Flex direction="column" gap="2">
 			<Text className="font-lato text-secondary-text">Hello from Radix Themes :)</Text>
 			<button>Let's go</button>
