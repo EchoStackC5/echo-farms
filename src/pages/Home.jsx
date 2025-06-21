660509a453afd3558fd207cb4c28c6986a7cf125
Gloria Dedo <gloriadedo100@gmail.com>
2025-06-20 23:29:58 -0700

    feat: added fonts and assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home(){
         <div>
             <p className="text-red-400 text-shadow-indigo-300 font-extrabold text-9xl font-poppins">This is Home</p>
             <Flex direction="column" gap="2">
-			<Text className="font-lato text-secondary-text">Hello from Radix Themes :)</Text>
+			<Text className="font-lato text-secondary-text">Hello from Radix Themes </Text>
 			<button>Let's go</button>
 		</Flex>
         </div>
