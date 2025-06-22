2d4dc18eb2e93fd8e72c8d1120300be12350b37a
Ayimaah <ayimaahtheodora2022@gmail.com>
2025-06-21 18:11:42 -0700

    added navbar

--- src/components/ProductNavbar.jsx
+++ src/components/ProductNavbar.jsx
@@ -5,6 +5,9 @@ export default function ProductNavBar(){
         <div>
             <ProductSearchBar/>
             <p className=" text-shadow-indigo-300 font-extrabold text-9xl">This is product Navbar</p>
+            <nav>
+                <h1>hello people</h1>
+            </nav>
         </div>
     )
 }
